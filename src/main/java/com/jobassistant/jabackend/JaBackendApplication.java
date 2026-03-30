d39ae0a0307cb8096503478d793c874b4a312b63
JungHyeonKim <dev.kjung@gmail.com>
2026-03-30 13:53:59 +0900

    [Feat]: Create project

---
+++ src/main/java/com/jobassistant/jabackend/JaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobassistant.jabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobassistant/jabackend/JaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobassistant.jabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
